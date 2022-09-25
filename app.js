a7f784761f85c9b19c5d60efb9aa52e7426c33ba
deligo21 <arrp1239@gmail.com>
2022-09-24 23:46:30 -0400

    mostrar menu y pausa

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-import 'colors';
+import colors from 'colors';
+import { mostrarMenu, pausa } from './helpers/mensajes.js';
+
+console.clear();
+
+const main = async() => {
+    console.log('Hola Mundo');
+
+    mostrarMenu();
+
+    pausa();
+}
+
+main();
